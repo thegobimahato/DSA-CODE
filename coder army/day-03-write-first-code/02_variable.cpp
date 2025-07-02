b483528f005da69a8ee6c47748393503d5bcffd8
Gobi Mahato <thegobimahato@gmail.com>
2025-07-02 13:31:30 +0530

    Coder Army | Day 03 - write your first code

---
+++ coder army/day-03-write-first-code/02_variable.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // variable declare
+  int a = 10, b = 20, d, e = 11; // int -> 4 byte
+  cout << a + b << endl;
+
+  char c = 'a'; // char -> 1 byte
+  cout << c << endl;
+
+  float f = 2.31; // float -> 4 byte
+  cout << f << endl;
+
+  double dd = 3.1212345; // double -> 8 bye
+  cout << dd << endl;
+
+  bool isAdmin = true; // boolean -> 1 byte
+  cout << isAdmin << endl;
+
+  return 0;
+}
